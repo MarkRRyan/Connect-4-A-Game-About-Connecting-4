423ad8a4ec7a87c22eac1c80fd1bd9eb2c0d40e3
Mark Ryan <markrryan99@gmail.com>
2022-03-08 12:09:39 -0800

    got the game pieces to finally show up on the board

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const gameboard = document.getElementById('gameboard');
-const cell = document.getElementsByTagName('td');
-const row = document.getElementsByTagName('tr');
 const spaceBoard = document.querySelector('.space');
-playerOne = 'Blue'
-playerTwo = 'Green'
+const spaceRow = document.getElementsByTagName('tr');
+const spaceCell = document.getElementsByTagName('td');
+let playerTurn = document.querySelector('.player-turn');
+const resetBtn = document.querySelector('.reset');
+const playerOne = 'Blue'
+const playerTwo = 'Green'
+const playerOneColor = 'blue';
+const playerTwoColor = 'green';
 
 
-for (let i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', (coord) => {
-        console.log(`${coord.target.parentElement.rowIndex}, ${coord.target.cellIndex}`);
-    })
-}
+for (i = 0; i < spaceCell.length; i ++){
+    spaceCell[i].addEventListener('click', (coord) =>{
+        console.log(`${coord.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
 
 let currentPlayer = playerOne;
-playerTurn.innerHTML = `${currentPlayer}'s Turn`
+// playerTurn.innerHTML = `${playerOne}'s turn!` 'broken code'
+
+Array.prototype.forEach.call(spaceCell, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = 'white';
+});
+
+function changeColor(coord) {
+    let column = coord.target.cellIndex;
+    let row = [];
+
+    for (let i = 5; i > -1; i--) {
+        if (spaceRow[i].children[column].style.backgroundColor == 'white'){            
+            row.push(spaceRow[i].children[column]);
+            if (currentPlayer === playerOne) {
+                row[0].style.backgroundColor = 'blue';
+                // playerTurn.innerHTML = `${playerTwo}'s Turn`; 'broken code'
+                return currentPlayer = playerTwo;
+            } else {
+                row[0].style.backgroundColor = 'green';
+                return currentPlayer = playerOne;
+            }
+        }
+    }
+}
