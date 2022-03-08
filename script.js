356c24c66e38f5f5d08be8b0af9ae8119c983a5f
Mark Ryan <markrryan99@gmail.com>
2022-03-08 06:10:15 -0800

    added starting vars and can locate the coordinate of each game space

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hi')
+const gameboard = document.getElementById('gameboard');
+const cell = document.getElementsByTagName('td');
+const row = document.getElementsByTagName('tr');
+const spaceBoard = document.querySelector('.space');
+playerOne = 'Blue'
+playerTwo = 'Green'
+
+
+for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('click', (coord) => {
+        console.log(`${coord.target.parentElement.rowIndex}, ${coord.target.cellIndex}`);
+    })
+}
+
+let currentPlayer = playerOne;
+playerTurn.innerHTML = `${currentPlayer}'s Turn`
